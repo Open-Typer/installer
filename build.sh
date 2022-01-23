b49bde876c39ee4a14a870306f3e9ae86fb5cf24
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-01-23 17:47:33 +0100

    build: Use -D option to delete repository branch

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ $BINARYCREATOR --offline-only -c config/config.xml -p packages Open-Typer-instal
 
 echo "Generating repository..."
 $REPOGEN -p packages repository || exit $?
-git branch -d repository
+git branch -D repository
 git switch --orphan repository &&
 mv repository/* ./ &&
 rm -rf repository &&
